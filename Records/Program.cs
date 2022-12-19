05d7e95ff3d9c27921643ecfba204d95cea4fc96
Umut Batuhan ALPAN <alpanbatuhanumut@gmail.com>
2022-12-19 18:21:46 +0300

    19/12/2022

--- Records/Program.cs
+++ Records/Program.cs
@@ -17,9 +17,22 @@ namespace OOPIntro_
             #region Init-Only Property
             InitOnly i1 = new InitOnly
             {
-                Adi = "Umut" //Object İnitializer ile sorunsuz bir şekilde ilk değer ataması yapabiliriz.
+                Adi = "Umut", //Object İnitializer ile sorunsuz bir şekilde ilk değer ataması yapabiliriz.
+                Afield = 5
             };
             // i1.Adi = "Batuhan"; // bu şekilde set edemeyiz çünkü ready only
+            #endregion
+            #region Record
+            MyRecord employee1 = new MyRecord   //Record oluşturma ve stack de referans olarak işaretleme
+            {
+                Afield= 5,      //readonly olan field'a encapsulation (Afield) üzerinden init ile başlangıç değeri ataması
+                Name = "Gençay",
+                Surname = "YILDIZ",         //record yapısında object initializers kullanımı da yapılabilmektedir.
+                Position= 1
+            };
+             // recordlarda bulunan sabit yapıdaki propertylerde herhangi bir değeri değiştirmek zorunda kalırsak kolaylıkla kullanabileceğimiz "with expression" özelliğinin kullanımı
+            MyRecord employee2 = employee1 with {  Position= 2 };
+            MyRecord employee3 =employee1 with { Name = "Umut" , Position= 3 }; 
             #endregion
         }
         #region ReadOnlyClass
@@ -32,6 +45,28 @@ namespace OOPIntro_
         class InitOnly
         {
             public string Adi { get; init; } // init; keyword'u sayesinde artık bu property init only property olarak davranacaktır.
+
+            // init ile readonly olan field'a değer atama durumu
+            readonly int a;
+            public int Afield { get { return a;} init { a = value; } }  
+
+        }
+        #endregion
+        #region MyRecord
+        //Record'larda birer class'dır classMember'ların hepsini bir record içerisinde de tanımlanabilir
+        record MyRecord
+        {
+            readonly int a;
+
+            public int Afield
+            {
+                get { return a; }
+                init { a = value; }
+            }
+
+            public string Name { get; init; }       //init yapısı object initializer esnasında başlangıç değeri atanıp daha sonrasında readonly olarak davranmasını sağlıyoruz
+            public string Surname { get; init; }        //recordlarda set'de kullanılabilir fakat init metotunun kullanılması daha doğrudur
+            public int Position { get; init; }
         }
         #endregion
     }
---
+++ SpecialClassMembers/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace OOPIntro_
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            #region Constructors
+            //new ConstructorClass();
+            //new ConstructorClass(5);
+            //new ConstructorClass("umut",5);
+
+            ////new PrivateConstructor();     private olarak tanımlanmış bir constructorMetot'a erişim olamıyor ve nesne oluşturulamıyor
+            ////PrivateConstructor m1 = new PrivateConstructor();
+
+            //ConstructorClass m = new(); //target-type ile nesne oluşturduk fakat constructor metot her daim tetiklenmek zorundadır (nesne oluşturulurken)
+            #endregion
+            #region Destructor
+            #region Senaryo1
+            // ! bilgi : GC sınıfı GarbageCollector'dır. ve System dosyalarının altında yer alır
+
+            // DestructorClass1 k1 = new DestructorClass1(); // --> eğer biz burada oluşturursak nesne gereksiz durumuna düşmüyor çünkü halen daha o nesneyi referans ile işaretleyen bir değişkene sahip main metotu
+
+            //X();
+            //GC.Collect(); // bu garbagecollector'ı çağırıp tüm gereksiz nesneleri temizle dediğimiz satır
+            //Console.ReadLine(); //readline yaptık çünkü programımız kısa olduğu için daha destructor çalışamadan program sonlanıyor
+            #endregion
+            #region Senaryo2
+            //int sayi = 110;
+            //while (sayi >= 1)
+            //{
+            //    new DestructorClass2(sayi);
+            //    sayi--;
+            //}
+            //Console.WriteLine("************************************");
+
+            //GC.Collect();
+
+            //Console.ReadLine();
+            #endregion
+            #endregion
+            #region StaticConstructor
+            new stConstructor();
+            new stConstructor();
+            #endregion
+        }
+
+        static void X()
+        {
+            DestructorClass1 m = new DestructorClass1();
+        }
+
+        #region Constructors
+        class ConstructorClass
+        {
+            // constructor metotların isimleri bulundukları class isimleri ile aynı isimde olmak zorundadır
+            // constructor metotların tanımlanmasında geri dönüş değeri hiç bahsedilmez (void dahil) direkt olarak metot ismi ile yazılır
+            int a;
+            public ConstructorClass()
+            {
+                Console.WriteLine("Bir tane ConstructorClass nesnesi oluşturulmuştur.");
+                a = 5; 
+            }
+            public ConstructorClass(int a)
+            {
+                //...
+            }
+            public ConstructorClass(string b , int a)
+            {
+                //... Constructor metotlar oveload yapılabilir
+            }
+            public void X(int t)
+            {
+                this.a = t;
+            }
+        }
+        #region this Keyword ile constructor tetikleme
+        // this keyword ile birden fazla constructor'lı bir yapıda diğer constructorların da tetiklenmesi sağlanabilmekte
+        class MyClass
+        {
+            public MyClass()
+            {
+                Console.WriteLine("1.Constructor");
+            }
+            public MyClass(int a) : this()
+            {
+                Console.WriteLine("2.Constructor");
+            }
+            public MyClass(int a, string b) : this(a)
+            { 
+                Console.WriteLine("3.Constructor");
+            }
+        }
+        #endregion
+        class PrivateConstructor
+        {
+            private PrivateConstructor()
+            {
+                //Dışarıdan nesne oluşturulmasını engelleyip içeriden oluşturulmaya devam edilebilir.
+
+            }
+
+            PrivateConstructor m1 = new PrivateConstructor();
+        }
+        #endregion
+        #region Destructor
+        #region Senaryo1
+        class DestructorClass1
+        {
+            public DestructorClass1()
+            {
+                Console.WriteLine("Nesne üretilmiştir.");
+            }
+            ~DestructorClass1()
+            {
+                Console.WriteLine("Nesne imha ediliyor...");
+            }
+        }
+        #endregion
+        #region Senaryo2
+        class DestructorClass2
+        {
+            int no;
+            public DestructorClass2(int no)
+            {
+                this.no = no;
+                Console.WriteLine($"{no}. nesne oluşturulmuştur.");
+            }
+
+            ~DestructorClass2()
+            {
+                Console.WriteLine($"{no}. nesne imha ediliyor...");
+            }
+        }
+        #endregion
+        #endregion
+        #region StaticConstructor
+        class stConstructor
+        {
+            static stConstructor()
+            {
+                //bu static constructor metot
+                Console.WriteLine("İlk nesne oluşturulurken ilk tetiklenen metot.");
+            }
+            public stConstructor()
+            {
+                //bu constructor metot
+                Console.WriteLine("Nesne oluşturulurken ilk tetiklenen metot."); //sadece ilk nesne oluşturulurken veya class içerisinde static yapıdaki herhangi bir member'ın ilk tetiklenmesi esnasında
+                //birinciliği static constructor'a kaptırır
+            }
+        }
+        #endregion
+    }
+}
